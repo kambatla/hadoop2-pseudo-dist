328a855dcfea9c2de20f59afa68dad5ae70c22f3
Karthik Kambatla <kasha@apache.org>
2015-11-17 15:15:42 -0800

    Add support for running SLS

---
+++ run-sls.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+INPUT=$1
+OUTPUT=$2
+
+printUsage() {
+    echo "Usage: ./run-sls.sh <sls-input> <sls-output>"
+}
+
+if [[ -z $INPUT ]]; then
+    printUsage
+fi
+
+if [[ -z $OUTPUT ]]; then
+    OUTPUT=sls-ouput
+fi
+
+# Load the configuration
+source ./configure.sh
+VERSION=$HADOOP_INSTALL_VERSION
+
+# Set PATH
+export PATH=$VERSION/bin:$PATH
+
+$VERSION/share/hadoop/tools/sls/bin/slsrun.sh --input-sls=$INPUT --output-dir=$OUTPUT --nodes="sls-traces/nodes-file"
