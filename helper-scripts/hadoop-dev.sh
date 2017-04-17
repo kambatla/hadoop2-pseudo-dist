ad67592508660e01da8582de2605f5aed1a695e3
Karthik Kambatla <kasha@cloudera.com>
2017-04-16 23:07:25 -0700

    branch-2 from master

--- helper-scripts/hadoop-dev.sh
+++ helper-scripts/hadoop-dev.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-set -e
+set -xe
 
 source hadoop-dev-env.sh
-
-set -x
-bin/hdfs --daemon $1 namenode
-bin/hdfs --daemon $1 datanode
-bin/yarn --daemon $1 resourcemanager
-bin/yarn --daemon $1 nodemanager
-bin/mapred --daemon $1 historyserver
+sbin/hadoop-daemon.sh $1 namenode
+sbin/hadoop-daemon.sh $1 datanode
+sbin/yarn-daemon.sh $1 resourcemanager
+sbin/yarn-daemon.sh $1 nodemanager
+sbin/mr-jobhistory-daemon.sh $1 historyserver
 #sbin/httpfs.sh $1
-set +x
-
 jps
