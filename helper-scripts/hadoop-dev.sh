d75d34f2a0109c1a547ca101af9fda6626ce4ba6
Karthik Kambatla <kasha@cloudera.com>
2017-04-16 23:00:38 -0700

    Changes specific to trunk - Use bin/hdfs etc. commands - Set properties in mapred-site to handle MR AM environment

--- helper-scripts/hadoop-dev.sh
+++ helper-scripts/hadoop-dev.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-set -xe
+set -e
 
 source hadoop-dev-env.sh
-sbin/hadoop-daemon.sh $1 namenode
-sbin/hadoop-daemon.sh $1 datanode
-sbin/yarn-daemon.sh $1 resourcemanager
-sbin/yarn-daemon.sh $1 nodemanager
-sbin/mr-jobhistory-daemon.sh $1 historyserver
+
+set -x
+bin/hdfs --daemon $1 namenode
+bin/hdfs --daemon $1 datanode
+bin/yarn --daemon $1 resourcemanager
+bin/yarn --daemon $1 nodemanager
+bin/mapred --daemon $1 historyserver
 #sbin/httpfs.sh $1
+set +x
+
 jps
